4bf2ea36781d62c0daf94b5640f8c2ce32465f58
henry <henryyogman@gmail.com>
2016-10-01 16:26:48 +0200

    simple bp for signin and signout

--- src/components/signin.js
+++ src/components/signin.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class Signin extends Component {
+
   render() {
+    const { handleSubmit } = this.props;
     return (
-      <div> SignIn form </div>
+      <div>
+        <h1>Sign in</h1>
+        <form onSubmit={handleSubmit}>
+         <fieldset className="form-group">
+           <label htmlFor="email">Email:</label>
+           <Field name="email" component="input" className="form-control" />
+         </fieldset>
+         <fieldset className="form-group">
+           <label htmlFor="password">Password:</label>
+           <Field name="password" component="input" className="form-control" />
+         </fieldset>
+         <button action="submit" className="btn btn-primary">Sign in</button>
+       </form>
+     </div>
     );
   }
 }
 
-export default Signin;
+export default Signin = reduxForm({
+  form: 'contact' 
+})(Signin);
--- src/components/signup.js
+++ src/components/signup.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class Signup extends Component {
+
   render() {
+    const { handleSubmit } = this.props;
     return (
-      <div> SignUp form </div>
+      <div>
+        <h1>Sign up</h1>
+        <form onSubmit={handleSubmit}>
+         <fieldset className="form-group">
+           <label htmlFor="email">Email:</label>
+           <Field name="email" component="input" className="form-control" />
+         </fieldset>
+         <fieldset className="form-group">
+           <label htmlFor="password">Password:</label>
+           <Field name="password" component="input" className="form-control" />
+         </fieldset>
+         <fieldset className="form-group">
+           <label htmlFor="confirmPassword">Confirm Password:</label>
+           <Field name="confirmPassword" component="input" className="form-control" />
+         </fieldset>
+         <button action="submit" className="btn btn-primary">Sign up</button>
+       </form>
+     </div>
     );
   }
 }
 
-export default Signup;
+export default Signup = reduxForm({
+  form: 'contact'
+})(Signup);
