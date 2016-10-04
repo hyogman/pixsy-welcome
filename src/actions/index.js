f5dedba7fac2525ab758449344e7e6cd6edc0951
henry <henryyogman@gmail.com>
2016-10-04 10:23:16 +0200

    signup with redux from basic console log

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,6 @@ const AUTH_USER = 'AUTH_USER';
 
 const ROOT_URL = 'http://localhost:3090';
 
-export const signupUser = ({email, password}) => {
+export const signinUser = ({email, password}) => {
   dispatch({ type: AUTH_USER });
 }
--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, Route, browserHistory } from 'react-router'
+import createSagaMiddleware from 'redux-saga';
 
 import App from './components/app';
 import Signup from './components/signup';
 import Signin from './components/signin';
 import reducers from './reducers';
+import mySaga from './sagas/saga';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(sagaMiddleware)(createStore);
+
+sagaMiddleware.run(mySaga);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
